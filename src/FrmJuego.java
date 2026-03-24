12a9b60804fb0e3eaa454763f054804081b61385
David Torres <david.torres1@udea.edu.co>
2026-03-24 00:00:29 -0500

    Proyecto terminado con lo pedido en el ejercicio 4 del taller 2

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -1,6 +1,4 @@
 import java.awt.Color;
-import java.util.Random;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
--- src/Jugador.java
+++ src/Jugador.java
@@ -30,6 +30,7 @@ public class Jugador {
     }
 
     public String getGrupos() {
+
         int[] contadores = new int[NombreCarta.values().length];
 
         for (Carta carta : cartas) {
@@ -37,14 +38,112 @@ public class Jugador {
         }
 
         String resultado = "";
-        for (int i = 0; i <contadores.length; i++) {
+
+        for (int i = 0; i < contadores.length; i++) {
             if (contadores[i] >= 2) {
                 resultado += Grupo.values()[contadores[i]] + " de " + NombreCarta.values()[i] + "\n";
             }
         }
 
+        for (int i = 0; i < TOTAL_CARTAS; i++) {
+
+            Carta base = cartas[i];
+            int valorBase = base.getNombre().ordinal();
+
+            boolean esInicio = true;
+
+            for (int j = 0; j < TOTAL_CARTAS; j++) {
+                if (cartas[j].getPinta() == base.getPinta()
+                        && cartas[j].getNombre().ordinal() == valorBase - 1) {
+                    esInicio = false;
+                }
+            }
+
+            if (!esInicio)
+                continue;
+
+            int cantidad = 1;
+            int actual = valorBase;
+
+            boolean encontrado = true;
+
+            while (encontrado) {
+                encontrado = false;
+
+                for (int j = 0; j < TOTAL_CARTAS; j++) {
+
+                    if (cartas[j].getPinta() == base.getPinta()) {
+
+                        int valorJ = cartas[j].getNombre().ordinal();
+
+                        if (valorJ == actual + 1 || (actual == 12 && valorJ == 0)) {
+                            cantidad++;
+                            actual = valorJ;
+                            encontrado = true;
+                        }
+                    }
+                }
+            }
+
+            if (cantidad >= 2) {
+                resultado += Grupo.values()[cantidad] + " de " + base.getPinta()
+                        + " de " + NombreCarta.values()[valorBase]
+                        + " a " + NombreCarta.values()[actual] + "\n";
+            }
+        }
+
+        int puntaje = 0;
+
+        for (int i = 0; i < TOTAL_CARTAS; i++) {
+
+            Carta actual = cartas[i];
+
+            int iguales = 0;
+            for (int j = 0; j < TOTAL_CARTAS; j++) {
+                if (cartas[j].getNombre() == actual.getNombre()) {
+                    iguales++;
+                }
+            }
+
+            boolean enEscalera = false;
+
+            for (int j = 0; j < TOTAL_CARTAS; j++) {
+
+                if (i != j) {
+
+                    Carta c1 = cartas[i];
+                    Carta c2 = cartas[j];
+
+                    if (c1.getPinta() == c2.getPinta()) {
+
+                        int n1 = c1.getNombre().ordinal();
+                        int n2 = c2.getNombre().ordinal();
+
+                        if (Math.abs(n1 - n2) == 1) {
+                            enEscalera = true;
+                        }
+                    }
+                }
+            }
+
+            if (iguales < 2 && !enEscalera) {
+
+                NombreCarta n = actual.getNombre();
+
+                if (n == NombreCarta.AS || n == NombreCarta.JACK
+                        || n == NombreCarta.QUEEN || n == NombreCarta.KING) {
+
+                    puntaje += 10;
+
+                } else {
+                    puntaje += n.ordinal() + 1;
+                }
+            }
+        }
+
+        resultado += "\nPuntos:\n" + puntaje;
+
         return resultado;
     }
-    
 
 }
